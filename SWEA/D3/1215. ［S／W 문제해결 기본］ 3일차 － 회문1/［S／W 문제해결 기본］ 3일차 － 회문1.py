f7d00bd66a74437af6bdec2150c043b19dda5469
이가은 <51986588+GaEun1216@users.noreply.github.com>
2024-05-15 16:39:13 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 113 ms, Memory: 45,232 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -1,22 +1,20 @@
 T = 10 #int(input())
-for test_case in range(1, T + 1):
-    length = int(input())
-    string = [ list(input()) for _ in range(8)]
-    string_2 = list(zip(*string))
 
-    result = []
+for test_case in range(1, T + 1):
+
+    length = int(input())
+    arr = [ list(input()) for _ in range(8) ]
+    brr = list(zip(*arr))
+    result = 0
     for i in range(8):
         for j in range(8-length+1):
-            tempstr = string[i][j:j+length]
-            tempstr2 = string_2[i][j:j+length]
-
-            if tempstr == tempstr[::-1]:
-                result.append(tempstr)
-            if tempstr2 == tempstr2[::-1]:
-                result.append(tempstr2)
-
-
-
-    print(f"#{test_case} {len(result)}")
+            
+            tmp1 = arr[i][j:j+length]
+            tmp2 = brr[i][j:j+length]
+            if tmp1 == tmp1[::-1]:
+                result += 1
+            if tmp2 == tmp2[::-1]:
+                result += 1
+    print(f"#{test_case} {result}")
 
 
